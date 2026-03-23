b5129295145d4d23c92afdae3b85cb4fb4340e46
justinvidual <justin.vidual@gmail.com>
2026-03-22 18:07:15 -0600

    ignore build temp files

--- .tmp-entries/abstract-loading-entry.js
+++
@@ -1,8 +0,0 @@
-import { mount, unmount } from 'svelte';
-import Component from '/home/justin/projects/friction-project/box-plugins/plugins/abstract-loading/src/AbstractLoadingBox.svelte';
-export function mountPlugin(target, props) {
-  return mount(Component, { target, props });
-}
-export function unmountPlugin(instance) {
-  unmount(instance);
-}
